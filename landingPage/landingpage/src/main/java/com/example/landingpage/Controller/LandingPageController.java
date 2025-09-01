fb5fdd89293025a96e3f99ca147e44355c4b4bdd
Santcar05 <castrogarzonsantiago@gmail.com>
2025-09-01 17:13:31 -0500

    header landingpage creado

---
+++ landingPage/landingpage/src/main/java/com/example/landingpage/Controller/LandingPageController.java
@@ -0,0 +1,15 @@
+package com.example.landingpage.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class LandingPageController {
+
+    @GetMapping
+    public String index() {
+        return "index";
+    }
+}
